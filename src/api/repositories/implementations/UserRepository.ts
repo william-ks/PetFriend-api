03c6c197443c2b684b1080de61d1bd01d29e8c33
William Krisley <will.ks2101@gmail.com>
2024-01-17 19:18:41 -0300

    create a user repository to handle with the database

---
+++ src/api/repositories/implementations/UserRepository.ts
@@ -0,0 +1,23 @@
+import { User } from "../../entities/User";
+import { IFindBy, IUserRepository } from "../IUserRepository";
+import { prisma } from "../../../config/prisma";
+
+export class UserRepository implements IUserRepository {
+  async findBy({ key, value }: IFindBy): Promise<User> {
+    const user = await prisma.user.findFirst({
+      where: {
+        [key]: value,
+      },
+    });
+
+    return user;
+  }
+
+  async save(props: Omit<User, "id">): Promise<void> {
+    await prisma.user.create({
+      data: {
+        ...props,
+      },
+    });
+  }
+}
