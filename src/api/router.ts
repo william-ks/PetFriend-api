e88fd9e1b4ebfaf3bed0c923ad903c8304f75759
William Krisley <will.ks2101@gmail.com>
2024-01-18 00:29:57 -0300

    router config

--- src/api/router.ts
+++ src/api/router.ts
@@ -1,46 +1,8 @@
-import { Router, Request, Response } from "express";
-import multer from "multer";
-import uploadConfig from "../config/multer";
-import { CreateUserController } from "./controllers/user/CreateUserController";
-import { AuthUserController } from "./controllers/user/AuthUserController";
-import { isAuthenticated } from "./middlewares/isAuthenticated";
-import { DetailPetsController } from "./controllers/pet/DetailPetsController";
-import { DetailUserController } from "./controllers/user/DetailUserController";
-import { CreatePetController } from "./controllers/pet/CreatePetController";
-import { GetPetsController } from "./controllers/pet/GetPetsController";
-import { DeletePetController } from "./controllers/pet/DeletePetController";
-import { DetailPetController } from "./controllers/pet/DetailPetController";
-import { GetPetsFilteredController } from "./controllers/pet/GetPetsFilteredController";
+import { Router } from "express";
+import { userRouter } from "./useCases/UserUseCases/user.routes";
 
-export const router = Router();
-const upload = multer(uploadConfig.upload("./tmp"));
+const router = Router();
 
-router.get("/", async (request: Request, response: Response) => {
-  return response.json({ ok: true });
-});
+router.use(userRouter);
 
-//User routes
-router.post("/register", new CreateUserController().handle);
-router.post("/login", new AuthUserController().handle);
-router.get("/me", isAuthenticated, new DetailUserController().handle);
-
-//Pet routes
-router.get("/pets", new DetailPetsController().handle);
-router.get("/detailPet/:pet_id", new DetailPetController().handle);
-router.get("/mypets", isAuthenticated, new GetPetsController().handle);
-router.get(
-  "/pets/:specie",
-  isAuthenticated,
-  new GetPetsFilteredController().handle
-);
-router.post(
-  "/pet/new",
-  isAuthenticated,
-  upload.single("file"),
-  new CreatePetController().handle
-);
-router.delete(
-  "/delete/:pet_id",
-  isAuthenticated,
-  new DeletePetController().handle
-);
+export { router };
