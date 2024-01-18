c2271fb5721e1ade03ac73a8c169c5f6024b80e6
William Krisley <will.ks2101@gmail.com>
2024-01-18 00:29:28 -0300

    adjust User entity

--- src/api/entities/User.ts
+++ src/api/entities/User.ts
@@ -1,12 +1,12 @@
 import { uuid } from "uuidv4";
 
 export class User {
-  public readonly id: string | number;
+  public readonly id: string;
   public name: string;
   public email: string;
   public password: string;
 
-  constructor(props: Omit<User, "id">, id?: string | number) {
+  constructor(props: Omit<User, "id">, id?: string) {
     Object.assign(this, props);
 
     if (!id) {
